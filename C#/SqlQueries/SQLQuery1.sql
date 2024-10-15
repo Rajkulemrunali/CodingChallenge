94542a0b3e908ebcd4e3594b26a3b9351283e58b
Rajkulemrunali <rajkulemrunalir104@gmail.com>
2024-10-15 12:19:14 +0530

    Insurance Management System

--- C#/SqlQueries/SQLQuery1.sql
+++
@@ -1,2 +0,0 @@
-﻿INSERT INTO Customers (CustomerName, Email, PhoneNumber) 
-VALUES ('John Doe', 'john.doe@example.com', 1234567890);
--- C#/SqlQueries/SQLQuery2.sql
+++
@@ -1,3 +0,0 @@
-﻿
-INSERT INTO Policy (userId,clientId,claimId,paymentId ) 
-VALUES (4,4,4,4)
